fe4af42656e4b2f5e7e7bda75e03f96f93f49bd6
HardCorePawn <hardcorepawn69@gmail.com>
2021-09-05 20:59:49 +1200

    Update generate_mnemonic_keysSOL.py

--- generate_mnemonic_keysSOL.py
+++ generate_mnemonic_keysSOL.py
@@ -1,6 +1,7 @@
 #
 # Copyright (c) 2013 Pavol Rusnak
 # Copyright (c) 2017 mruddy
+# Copyright (c) 2021 HCP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
